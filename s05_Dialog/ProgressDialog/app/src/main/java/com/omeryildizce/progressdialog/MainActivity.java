f66167d9049ea81002f7d5295c7218d15a9d5d2d
omeryildizce <omeryildizce@gmail.com>
2023-02-04 01:00:23 +0300

    Update MainActivity.java

--- s05_Dialog/ProgressDialog/app/src/main/java/com/omeryildizce/progressdialog/MainActivity.java
+++ s05_Dialog/ProgressDialog/app/src/main/java/com/omeryildizce/progressdialog/MainActivity.java
@@ -2,7 +2,9 @@ package com.omeryildizce.progressdialog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dialogVer();
+    }
+
+    private void dialogVer() {
+        ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Mesajlar bölümü");
+        progressDialog.setMessage("Mesajlar Listeleniyor, Lütfen bekleyin!");
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(10_000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                progressDialog.dismiss();
+            }
+        }).start();
     }
 }
