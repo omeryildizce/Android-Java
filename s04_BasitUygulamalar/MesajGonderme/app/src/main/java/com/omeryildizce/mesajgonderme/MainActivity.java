dd9a7a7d7e9ad484738a29dbe4652d9420ffdfc3
omeryildizce <omeryildizce@gmail.com>
2023-02-03 23:17:23 +0300

    Update MainActivity.java

--- s04_BasitUygulamalar/MesajGonderme/app/src/main/java/com/omeryildizce/mesajgonderme/MainActivity.java
+++ s04_BasitUygulamalar/MesajGonderme/app/src/main/java/com/omeryildizce/mesajgonderme/MainActivity.java
@@ -3,12 +3,40 @@ package com.omeryildizce.mesajgonderme;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    List<Model> list;
+    ListView listView;
+    Adapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listeDoldur();
+        tanimla();
+    }
+
+    private void tanimla() {
+        listView = findViewById(R.id.mesajList);
+        adapter = new Adapter(list, this);
+        listView.setAdapter(adapter);
+    }
+
+    private void listeDoldur() {
+        list = new ArrayList<>();
+        Model m1 = new Model("Murat", "054000000000");
+        Model m2 = new Model("Başar", "054000000000");
+        Model m3 = new Model("Ege", "054000000000");
+        Model m4 = new Model("Tamer", "054000000000");
+        Model m5 = new Model("Kader", "054000000000");
+        list.add(m1);
+        list.add(m2);
+        list.add(m3);
+        list.add(m4);
+        list.add(m5);
     }
 }
