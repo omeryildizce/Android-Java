97d4d258b1a2543a7ee153def3161bc8d189bccd
omeryildizce <omeryildizce@gmail.com>
2023-02-03 19:58:59 +0300

    Update MainActivity.java

--- s03_Activity/ActivityArasiVeriGonderme/app/src/main/java/com/omeryildizce/activityarasiverigonderme/MainActivity.java
+++ s03_Activity/ActivityArasiVeriGonderme/app/src/main/java/com/omeryildizce/activityarasiverigonderme/MainActivity.java
@@ -2,13 +2,55 @@ package com.omeryildizce.activityarasiverigonderme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText kullaniciAdi, kullaniciSifre;
+    Button girisYap;
+    RadioGroup radioCinsiyet;
+    RadioButton radioButton;
+    String isim, sifre, cinsiyet;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tanimla();
+        tiklaVeGec();
+    }
+
+    private void tiklaVeGec() {
+        girisYap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                degerAl();
+                Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+                intent.putExtra("kullaniciAdi", isim);
+                intent.putExtra("kullaniciSifre", sifre);
+                intent.putExtra("kullaniciCinsiyet", cinsiyet);
+                startActivity(intent);
+            }
+        });
+     }
+
+    private void degerAl() {
+        isim = kullaniciAdi.getText().toString();
+        sifre = kullaniciSifre.getText().toString();
+       radioButton = findViewById(radioCinsiyet.getCheckedRadioButtonId());
+       cinsiyet = radioButton.getText().toString();
+
+    }
+
+    private void tanimla() {
+        kullaniciAdi = findViewById(R.id.editTextKullanici);
+        kullaniciSifre = findViewById(R.id.editTextSifre);
+        girisYap = findViewById(R.id.buttonGiris);
+        radioCinsiyet = findViewById(R.id.cinnsiyetGroup);
+
     }
 }
