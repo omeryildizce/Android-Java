7e54ade583aec68296764536a367e1fdc634b26b
omeryildizce <omeryildizce@gmail.com>
2023-02-03 16:45:06 +0300

    Update MainActivity.java

--- s03_Activity/MyActivity/app/src/main/java/com/omeryildizce/myactivity/MainActivity.java
+++ s03_Activity/MyActivity/app/src/main/java/com/omeryildizce/myactivity/MainActivity.java
@@ -3,6 +3,7 @@ package com.omeryildizce.myactivity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("Takip: ", "onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("Takip: ", "onStart");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("Takip: ", "onResume");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("Takip: ", "onPause");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("Takip: ", "onStop");
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("Takip: ", "onRestart");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("Takip: ", "onDestroy");
+
     }
 }
