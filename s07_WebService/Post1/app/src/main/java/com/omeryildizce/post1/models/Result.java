da4d5a77e3f27c27b461fcbef242984a99d938e5
omeryildizce <omeryildizce@gmail.com>
2023-02-06 22:02:15 +0300

    Create Result.java

---
+++ s07_WebService/Post1/app/src/main/java/com/omeryildizce/post1/models/Result.java
@@ -0,0 +1,13 @@
+package com.omeryildizce.post1.models;
+
+public class Result {
+    private String Result;
+
+    public String getResult() {
+        return Result;
+    }
+
+    public void setResult(String result) {
+        Result = result;
+    }
+}
