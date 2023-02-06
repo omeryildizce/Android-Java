39fd08ebe855b7f6dd1380e11b85da111c4dc48f
omeryildizce <omeryildizce@gmail.com>
2023-02-06 16:51:14 +0300

    Create ManagerAll.java

---
+++ s07_WebService/WebServis3/app/src/main/java/com/omeryildizce/webservis3/restApi/ManagerAll.java
@@ -0,0 +1,19 @@
+package com.omeryildizce.webservis3.restApi;
+
+import com.omeryildizce.webservis3.models.Bilgi;
+
+import java.util.List;
+
+import retrofit2.Call;
+
+public class ManagerAll  extends BaseManager{
+    private static ManagerAll ourGetInstance = new ManagerAll();
+    public static synchronized ManagerAll getInstance()
+    {
+        return ourGetInstance;
+    }
+    public Call<List<Bilgi>> getirBilgi(){
+        Call<List<Bilgi>> x = getRestApiClient().getir();
+        return x;
+    }
+}
