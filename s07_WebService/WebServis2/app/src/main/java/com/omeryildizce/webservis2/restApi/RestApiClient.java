3b6747a6848288fc28b29442370730ad4f1b5304
omeryildizce <omeryildizce@gmail.com>
2023-02-06 13:35:58 +0300

    Create RestApiClient.java

---
+++ s07_WebService/WebServis2/app/src/main/java/com/omeryildizce/webservis2/restApi/RestApiClient.java
@@ -0,0 +1,27 @@
+package com.omeryildizce.webservis2.restApi;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RestApiClient {
+    private RestApi mRestApi;
+    public RestApiClient(String restApiServiceUrl){
+        OkHttpClient.Builder builder = new OkHttpClient.Builder()
+                .readTimeout(30, TimeUnit.SECONDS)
+                .writeTimeout(30, TimeUnit.SECONDS)
+                .connectTimeout(30, TimeUnit.SECONDS);
+        OkHttpClient okHttpClient = builder.build();
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(restApiServiceUrl)
+                .client(okHttpClient)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        mRestApi = retrofit.create(RestApi.class);
+    }
+    public RestApi getRestApi(){
+        return mRestApi;
+    }
+}
