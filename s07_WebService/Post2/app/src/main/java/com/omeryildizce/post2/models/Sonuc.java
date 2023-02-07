91999885b3c08bf896ef77f1d9e78e176489671d
omeryildizce <omeryildizce@gmail.com>
2023-02-07 16:48:12 +0300

    Create Sonuc.java

---
+++ s07_WebService/Post2/app/src/main/java/com/omeryildizce/post2/models/Sonuc.java
@@ -0,0 +1,13 @@
+package com.omeryildizce.post2.models;
+
+public class Sonuc {
+    private String result;
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+}
