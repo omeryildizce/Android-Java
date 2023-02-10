ccb9237550c84ec714151a039e6fdb5818cc0206
omeryildizce <omeryildizce@gmail.com>
2023-02-10 20:03:40 +0300

    Create Result.java

---
+++ s07_WebService/UploadServer/app/src/main/java/com/omeryildizce/uploadserver/models/Result.java
@@ -0,0 +1,20 @@
+package com.omeryildizce.uploadserver.models;
+
+public class Result {
+    private String result;
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    @Override
+    public String toString() {
+        return "Result{" +
+                "result='" + result + '\'' +
+                '}';
+    }
+}
