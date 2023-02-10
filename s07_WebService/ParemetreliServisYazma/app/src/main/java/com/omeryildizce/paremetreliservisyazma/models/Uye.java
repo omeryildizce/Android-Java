220a12f2f2bfe1f09d5569b2007689eee58ee9e0
omeryildizce <omeryildizce@gmail.com>
2023-02-10 20:21:06 +0300

    Create Uye.java

---
+++ s07_WebService/ParemetreliServisYazma/app/src/main/java/com/omeryildizce/paremetreliservisyazma/models/Uye.java
@@ -0,0 +1,41 @@
+package com.omeryildizce.paremetreliservisyazma.models;
+
+public class Uye {
+    private String id;
+    private String password;
+
+    @Override
+    public String toString() {
+        return "Uye{" +
+                "id='" + id + '\'' +
+                ", password='" + password + '\'' +
+                ", username='" + username + '\'' +
+                '}';
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    private String username;
+}
