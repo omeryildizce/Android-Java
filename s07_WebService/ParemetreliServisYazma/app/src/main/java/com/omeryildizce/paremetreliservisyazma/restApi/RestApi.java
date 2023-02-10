b2603f13b8f0bd5229dbbfd71e6fcb2f626f36ce
omeryildizce <omeryildizce@gmail.com>
2023-02-10 21:53:16 +0300

    Update RestApi.java

--- s07_WebService/ParemetreliServisYazma/app/src/main/java/com/omeryildizce/paremetreliservisyazma/restApi/RestApi.java
+++ s07_WebService/ParemetreliServisYazma/app/src/main/java/com/omeryildizce/paremetreliservisyazma/restApi/RestApi.java
@@ -1,15 +1,20 @@
 package com.omeryildizce.paremetreliservisyazma.restApi;
 import com.omeryildizce.paremetreliservisyazma.models.Uye;
-
-import java.util.List;
+import com.omeryildizce.paremetreliservisyazma.models.UyeBilgileri;
 
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Query;
 
 public interface RestApi {
     @FormUrlEncoded
     @POST("proje/girisyap.php")
-    Call<List<Uye>> addUser(@Field("username") String username, @Field("password")String password);
+    Call<Uye> kontrol(@Field("username") String username, @Field("password")String password);
+
+
+    @GET("proje/bilgiGetir.php")
+    Call<UyeBilgileri> bilgiGetir(@Query("uyeid") String uyeid);
 }
