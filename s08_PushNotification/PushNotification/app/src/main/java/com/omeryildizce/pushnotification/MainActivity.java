3c0b5d75d7c2780627fe4f271d6abed61e76d4b7
omeryildizce <omeryildizce@gmail.com>
2023-02-11 16:53:58 +0300

    Update MainActivity.java

--- s08_PushNotification/PushNotification/app/src/main/java/com/omeryildizce/pushnotification/MainActivity.java
+++ s08_PushNotification/PushNotification/app/src/main/java/com/omeryildizce/pushnotification/MainActivity.java
@@ -4,11 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.onesignal.OneSignal;
 
+public class MainActivity extends AppCompatActivity {
+    private static final String ONESIGNAL_APP_ID = "1";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE);
+        OneSignal.initWithContext(this);
+        OneSignal.setAppId(ONESIGNAL_APP_ID);
+        OneSignal.promptForPushNotifications();
+
+
+
+
     }
 }
